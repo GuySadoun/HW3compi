c7abeb3db56e5a2394643e7dde771a3cf0269ef1
guy-pc <sadoun90@gmail.com>
2019-12-18 18:09:40 +0200

    err type in semantic

--- semantic.cpp
+++ semantic.cpp
@@ -6,26 +6,73 @@
 using namespace output;
 
 void semantic::binop(Types &target, Types &a, Types &b, string sign, int lineno) {
-    if(a.Exp.isBool() && b.Exp.isBool()) {
-        target.Exp.val.boolean = a.Exp.val.boolean && b.Exp.val.boolean;
-        target.Exp.type = BOOL;
+    if ((a.Exp.isInt() && b.Exp.isInt()) || (a.Exp.isByte() && b.Exp.isByte()))  {
+        if (sign == "+") {
+            target.Exp.val.integer = a.Exp.val.integer + b.Exp.val.integer;
+            target.Exp.type = INT;
+        } else if (sign == "-") {
+            target.Exp.val.integer = a.Exp.val.integer - b.Exp.val.integer;
+            target.Exp.type = INT;
+        } else if (sign == "*") {
+            target.Exp.val.integer = a.Exp.val.integer * b.Exp.val.integer;
+            target.Exp.type = INT;
+        } else if (sign == "/") {
+            target.Exp.val.integer = a.Exp.val.integer / b.Exp.val.integer;
+            target.Exp.type = INT;
+        } else {
+            string exceptionMessage("binop undefined");
+            throw (semErr(exceptionMessage));
+        }
     } else
         errorSyn(lineno);
 }
 
 void semantic::relop(Types &target, Types &a, Types &b, string sign, int lineno) {
-    if ((a.Exp.isInt() && b.Exp.isInt()) || (a.Exp.isByte() && b.Exp.isByte())) {
-        target.Exp.type = BOOL;
-        target.Exp.val.boolean = (a.Exp.val.boolean == b.Exp.val.boolean);
+    if (a.Exp.isInt() && b.Exp.isInt()) {
+        if (sign == "<") {
+            target.Exp.val.boolean = (a.Exp.val.integer < b.Exp.val.integer);
+            target.Exp.type = BOOL;
+        } else if (sign == "<=") {
+            target.Exp.val.boolean = (a.Exp.val.integer <= b.Exp.val.integer);
+            target.Exp.type = BOOL;
+        } else if (sign == ">") {
+            target.Exp.val.boolean = (a.Exp.val.integer > b.Exp.val.integer);
+            target.Exp.type = BOOL;
+        } else if (sign == ">=") {
+            target.Exp.val.boolean = (a.Exp.val.integer >= b.Exp.val.integer);
+            target.Exp.type = BOOL;
+        } else {
+            string exceptionMessage("relop undefined");
+            throw (semErr(exceptionMessage));
+        }
     } else
         errorSyn(lineno);
 }
 
 void semantic::logicop(Types &target, Types &a, Types &b, string sign, int lineno) {
-
+    if (a.Exp.isBool() && b.Exp.isBool()) {
+        if (sign == "and") {
+            target.Exp.val.boolean = (a.Exp.val.boolean && b.Exp.val.boolean);
+            target.Exp.type = BOOL;
+        } else if (sign == "or") {
+            target.Exp.val.boolean = (a.Exp.val.boolean || b.Exp.val.boolean);
+            target.Exp.type = BOOL;
+        } else if (sign == "not") {
+            target.Exp.val.boolean = !a.Exp.val.boolean;
+            target.Exp.type = BOOL;
+        } else {
+            string exceptionMessage("logicop undefined");
+            throw (semErr(exceptionMessage));
+        }
+    } else
+        errorSyn(lineno);
 }
 
 void semantic::call(Types &target, Types &call, int lineno) {
+    target.Exp.type = call.
+}
+
+void semantic::bytecheck(Types &target, Types &byte, int lineno) {
 
 }
 
--- semantic.h
+++ semantic.h
@@ -13,6 +13,17 @@ enum ErrorType {
     SYNERR,
     LEXERR,
 };
+
+class semErr : public std::exception {
+public:
+    semErr(const string msg) : m_msg(msg) {}
+    const char* what() const throw () override {
+        cout << "semErr - what:" << m_msg << endl;
+        return m_msg.c_str();
+    }
+    const string m_msg;
+};
+
 using std::string;
 namespace semantic {
     void binop(Types& target, Types &a, Types &b, string sign, int lineno);
@@ -20,6 +31,6 @@ namespace semantic {
     void relop(Types& target, Types &a, Types &b, string sign, int lineno);
     void cast(Types& target, Types &a, Types &b, string sign, int lineno);
     void call(Types& target, Types &call, int lineno);
-    void enumdecl(Types &target, string name, string enumValues, int lineno);
+    void bytecheck(Types& target, Types &byte, int lineno);
 }
 #endif //HW3COMPI_SEMANTIC_H
